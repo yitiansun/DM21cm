08c4929b8a58c9138273ed4facd4ac7a389f8973
Yitian Sun <yitian.sun.26@gmail.com>
2023-02-18 11:35:05 -0500

    reionization

--- dm21cm/common.py
+++ dm21cm/common.py
@@ -53,3 +53,20 @@ abscs_nBs_test_2 = {
 
 def fitsfn(z, log10E, x, nBs, base=''):
     return f'{base}tf_z_{z:.3E}_logE_{log10E:.3E}_x_{x:.3E}_nBs_{nBs:.3E}.fits'
+
+def get_circle(size):
+    im = np.zeros((size, size))
+    for i in range(size):
+        for j in range(size):
+            if (i-(size-1)/2)**2 + (j-(size-1)/2)**2 < (0.3*size)**2:
+                im[i,j] = 1
+    return im
+
+def get_circle_seq_at(size, n_per_side, at_i):
+    bs = int(np.floor(size/n_per_side))
+    at_i = at_i % n_per_side**2
+    i = at_i // n_per_side
+    j = at_i %  n_per_side
+    im = np.zeros((size,size))
+    im[i*bs:(i+1)*bs, j*bs:(j+1)*bs] = get_circle(bs)
+    return np.einsum('i,jk->ijk', np.ones((size,)), im)

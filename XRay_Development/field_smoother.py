Fix weighting issue

XRay_Development/field_smoother.py
@@ -34,16 +34,29 @@ class WindowedData:
     def get_field(self, field_index):
         with h5py.File(self.data_path, 'r') as archive:
             return np.array(archive['Field_' + str(field_index)], dtype = complex), np.array(archive['Spec_' + str(field_index)], dtype = float)
-        
+
+
     def smoothed_shell(self, redshift, R1, R2):
         # Get the index of the nearest field in redshift
         field_index = np.argmin(np.abs(self.redshifts - redshift))
-        
+
         # Canonically sort the radii
         R1, R2 = np.sort([R1, R2])
+        w1 = R1**3 / (R2**3 - R1**3)
+        w2 = R2**3 / (R2**3 - R1**3)
+
+        # Construct the smoothing function 
+        W1 = 3*(np.sin(self.kMag*R1) - self.kMag*R1 * np.cos(self.kMag*R1)) /(self.kMag*R1)**3 
+        W2 = 3*(np.sin(self.kMag*R2) - self.kMag*R2 * np.cos(self.kMag*R2)) /(self.kMag*R2)**3 
+        
+        # Fix the nan issue
+        W1[0, 0, 0] = 1
+        W2[0, 0, 0] = 1
+
+        # Combine the window functions
+        W = w2*W2 - w1*W1
+        del W1, W2
 
         # Load the field and define the smoothing functions
         field, spec = self.get_field(field_index)
-        W = np.exp(-(self.kMag*R2)**2/2) - (R1/R2)**3*np.exp(-(self.kMag*R1)**2/2)
-        
         return np.fft.irfftn(field * W), spec
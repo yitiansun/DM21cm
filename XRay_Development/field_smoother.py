7d40ef4bdf365ab0e280761fe5fc385aac404007
joshwfoster <fosterjw@umich.edu>
2023-06-09 10:04:14 -0700

    Comoving coordinate correction

--- XRay_Development/field_smoother.py
+++ XRay_Development/field_smoother.py
@@ -1,6 +1,7 @@
 import sys, os, h5py
 import numpy as np
-from scipy import signal, ndimage, stats, interpolate
+from scipy import signal, ndimage, stats, interpolate, integrate
+from astropy import cosmology, constants, units
 
 class WindowedData:
     def __init__(self, data_path, cosmo, N, dx, N_x):
@@ -37,22 +38,29 @@ class WindowedData:
             return np.array(archive['Field_' + str(field_index)], dtype = complex), np.array(archive['Spec_' + str(field_index)], dtype = float)
 
     def get_smoothing_radii(self, z_receiver, z1, z2):
-        """
-        Returns the proper null geodesic distance between z_receiver and {z1, z2} 
-        in units of Mpc
-        """
+        z_at_t = lambda t: cosmology.z_at_value(self.cosmo.age, t)
+        integrand = lambda t: 1+z_at_t(t*units.Gyr)
 
-        R1 = self.cosmo.lookback_distance(z1) - self.cosmo.lookback_distance(z_receiver)
-        R2 = self.cosmo.lookback_distance(z2) - self.cosmo.lookback_distance(z_receiver)
+        t_receiver = self.cosmo.age(z_receiver).value
+        t1 = self.cosmo.age(z1).value
+        t2 = self.cosmo.age(z2).value
 
-        return R1.value, R2.value
+        R1 = integrate.quad(integrand, t_receiver, t1)[0] *constants.c.to(units.Mpc / units.Gyr)*units.Gyr
+        R2 = integrate.quad(integrand, t_receiver, t2)[0] *constants.c.to(units.Mpc / units.Gyr)*units.Gyr
+
+        return np.abs(R1.value), np.abs(R2.value)
 
     def get_smoothed_shell(z_receiver, z_donor, z_next_donor):
+        '''
+        Smooths the shells for the given specification of the receiver
+        redshift `z_receiver`, from a shell with outer radius defined by
+        `z_donor` and an inner radius defined by `z_next_donor`.
+        '''
 
         # Get the index of the donor field
         field_index = np.argmin(np.abs(self.redshifts - redshift))
 
-        # Get the smoothing radii and canonically sort them
+        # Get the smoothing radii in comoving coordinates and canonically sort them
         R1, R2 = get_smoothing_radii(self, z_receiver, z_donor, z_next_donor)
 
         # Volumetric weighting factors for combining the window functions
@@ -72,10 +80,14 @@ class WindowedData:
         W = w2*W2 - w1*W1
         del W1, W2
 
-        # Geometric factors for calculating X-rays incident on a point
+        # Comoving shell volume
         shell_volume = 4*np.pi/ 3 * (R2**3 - R1**3) # volume of the comoving shell
-        solid_angle = 1 / np.pi / (R2 + R1)**2 # the solid angle of the target 
+
+        # Target solid angle
+        Rp = self.cosmo.lookback_distance(z_donor) - self.cosmo.lookback_distance(z_receiver)
+        solid_angle = 1 / 4 / np.pi /Rp**2
 
         # Load the field and smooth via FFT
         field, spec = self.get_field(field_index)
         return shell_volume * solid_angle * np.fft.irfftn(field * W), spec
+

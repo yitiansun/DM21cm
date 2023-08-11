21cfccc1b62ba7d5700b711a91f8ff7358d335fe
Josh Foster <fosterjw@n0000.scs00>
2023-08-11 07:15:14 -0700

    Cleaner switches in photon injection method

--- dm21cm/dh_wrapper.py
+++ dm21cm/dh_wrapper.py
@@ -54,11 +54,11 @@ class DMParams:
         )
         self.eng_per_inj = self.m_DM if self.mode=='decay' else 2 * self.m_DM
         
-        
     def __repr__(self):
         
         return f"DMParams(mode={self.mode}, primary={self.primary}, "\
     f"m_DM={self.m_DM:.4e}, sigmav={self.sigmav}, lifetime={self.lifetime})"
+
 
 class DarkHistoryWrapper:
     
@@ -105,19 +105,14 @@ class DarkHistoryWrapper:
         self.phot_prop_tf = load_data('phot_prop', prefix=transfer_prefix, reload=self.force_reload_tf)
         self.phot_scat_tf = load_data('phot_scat', prefix=transfer_prefix, reload=self.force_reload_tf)
         self.phot_dep_tf = load_data('phot_dep', prefix=transfer_prefix, reload=self.force_reload_tf)
-        #self.phot_prop_tf.set_fixed_in_spec(dm_params.inj_phot_spec.N)
-        #self.phot_scat_tf.set_fixed_in_spec(dm_params.inj_phot_spec.N)
-        #self.phot_dep_tf.set_fixed_in_spec(dm_params.inj_phot_spec.N)
     
         # Initialize electron transfer functions if desired
         if self.enable_elec:
             raise NotImplementedError
             self.elec_phot_tf = load_data('elec_phot', prefix=transfer_prefix, reload=self.force_reload_tf)
             self.elec_dep_tf = load_data('elec_dep', prefix=transfer_prefix, reload=self.force_reload_tf)
-            #self.elec_phot_tf.set_fixed_in_spec(dm_params.inj_elec_spec.N)
-            #self.elec_dep_tf.set_fixed_in_spec(dm_params.inj_elec_spec.N)
             
-    def set_empty_arrays(self):
+    def set_empty_arrays(self, tf_kwargs):
         '''
         This is a convenience method that sets the properly shaped deposition box for 
         use in our evolution
@@ -131,13 +126,6 @@ class DarkHistoryWrapper:
         self.dep_box = np.zeros((self.HII_DIM, self.HII_DIM, self.HII_DIM, self.dep_size))
         
         # We will also set the tf_kwargs to None to raise an error if they have not been reset
-        self.tf_kwargs = None
-        
-    def set_tf_kwargs(self, tf_kwargs):
-        '''
-        Set the tf_kwargs
-        '''
-        
         self.tf_kwargs = tf_kwargs
 
     def get_state_arrays(self):
@@ -146,26 +134,28 @@ class DarkHistoryWrapper:
         This is a convenience method that provides the properly shaped deposition box for 
         use in our evolution
         '''
+
+        # We only access the data once we are finished calculating deposition. We will set 
+        # the tf_kwargs to None, which will result in an error if we try to deposit more
+        self.tf_kwargs = None
         
-        # Return these
+        # Return the deposition quantities
         return self.prop_phot_N, self.emit_phot_N, self.dep_box
-    
-    def photon_injection(self, spec_object, bath = True, weight_box = None, ots = False):
-        if bath:
-            self.prop_phot_N += self.norm*self.phot_prop_tf(in_spec=spec_object.N,
-                                                            sum_result=True,
-                                                            **self.tf_kwargs) # [N / Bavg]
+
+    def photon_injection(self, spec_object, injection_type = 'bath', weight_box = None):
+        if weight_box is None:
             weight_box = np.ones((self.HII_DIM, self.HII_DIM, self.HII_DIM)) # dummy
-            
-        if ots:
+
+        if injection_type == 'bath':
+            self.prop_phot_N += self.norm*self.phot_prop_tf(in_spec=spec_object.N, sum_result=True, **self.tf_kwargs) # [N / Bavg]
+
+        if injection_type == 'ots':
             self.emit_phot_N += self.norm*self.phot_prop_tf(in_spec = spec_object.N, sum_result = True,
                                                             sum_weight = weight_box.ravel(), **self.tf_kwargs)
-            
+
         self.emit_phot_N += self.norm*self.phot_scat_tf(in_spec=spec_object.N, sum_result=True,
                                                         sum_weight = weight_box.ravel(), **self.tf_kwargs)
         self.dep_box += weight_box[..., None] * self.phot_dep_tf(in_spec=spec_object.N, sum_result=False, **self.tf_kwargs).reshape(self.dep_box.shape) # [eV / Bavg]
-
-
 
     def populate_injection_boxes(self, input_heating, input_ionization, input_jalpha, x_e_box, delta_plus_one_box, nBavg):
     
@@ -180,4 +170,3 @@ class DarkHistoryWrapper:
         dnu_lya = (phys.rydberg - phys.lya_eng) / (2*np.pi*phys.hbar) # [Hz^-1]
         J_lya = n_lya * phys.c / (4*np.pi) / dnu_lya # [lya cm^-2 s^-1 sr^-1 Hz^-1]
         input_jalpha.input_jalpha += np.array(J_lya)
-    

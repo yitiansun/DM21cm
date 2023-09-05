cda91def3436b6a7f6458c16bc3c06500bd5198e
Yitian Sun <yitian.sun.26@gmail.com>
2023-09-05 13:46:53 -0400

    new maketf for electf

--- build_tf/2_gather_fits.py
+++ build_tf/2_gather_fits.py
@@ -12,14 +12,14 @@ from darkhistory.spec.spectools import get_log_bin_width
 
 
 #===== Config =====
-run_name = '230721'
-tf_type = 'phot' # {'phot', 'elec'}
+run_name = '230629'
+tf_type = 'elec' # {'phot', 'elec'}
 
 
 #===== Initialize =====
 abscs = load_dict(f"../data/abscissas/abscs_{run_name}.h5")
-fits_dir = f'../data/tf/{run_name}/{tf_type}/ionhist_outputs'
-save_dir = f'../data/tf/{run_name}/{tf_type}'
+fits_dir = f"{os.environ['DM21CM_DATA_DIR']}/tf/{run_name}/{tf_type}/ionhist_outputs"
+save_dir = f"{os.environ['DM21CM_DATA_DIR']}/tf/{run_name}/{tf_type}"
 in_absc = abscs['photE'] if tf_type == 'phot' else abscs['elecEk']
 
 # convention: rxneo : rs x nBs Ein out
@@ -32,6 +32,7 @@ lee_tf  = np.full(interp_shape + (len(in_absc), len(abscs['elecEk'])), EPSILON)
 hed_tf  = np.full(interp_shape + (len(in_absc), 4), 0.0)
 cmbloss = np.full(interp_shape + (len(in_absc),), 0.0)
 lowerbound = np.full(interp_shape, 0.0)
+dt = np.full((len(abscs['rs']), 2), 0.0)
 
 phot_bin_width = abscs['photE'] * get_log_bin_width(abscs['photE'])
 elec_bin_width = abscs['elecEk'] * get_log_bin_width(abscs['elecEk'])
@@ -42,15 +43,17 @@ pbar = tqdm(total=len(abscs['rs'])*len(abscs['x'])*len(abscs['nBs']))
 for i_rs, rs in enumerate(abscs['rs']):
     for i_x, x in enumerate(abscs['x']):
         for i_nBs, nBs in enumerate(abscs['nBs']):
-            # tmp force nBs = 1
-            nBs = 1.0
-            with fits.open(fitsfn(rs, x, nBs, prefix=fits_dir)) as f:
-                hep_tf[i_rs, i_x, i_nBs] = f[1].data['hep_tf'][0] * phot_bin_width
-                lep_tf[i_rs, i_x, i_nBs] = f[1].data['lep_tf'][0] * phot_bin_width
-                lee_tf[i_rs, i_x, i_nBs] = f[1].data['lee_tf'][0] * elec_bin_width
-                hed_tf[i_rs, i_x, i_nBs] = f[1].data['hed_tf'][0]
-                cmbloss[i_rs, i_x, i_nBs] = f[1].data['cmbloss'][0]
-                lowerbound[i_rs, i_x, i_nBs] = f[1].data['lowerbound'][0]
+            fitsfn = f"{fits_dir}/{tf_type}tf_rs{i_rs}_xx{i_x}_nB{i_nBs}.fits"
+            with fits.open(fitsfn) as f:
+                d = f[1].data
+                # d['name'][0][step]
+                hep_tf[i_rs, i_x, i_nBs] = np.einsum('ij,j->ij', d['hep_tf'][0][1], phot_bin_width) # N -> N
+                lep_tf[i_rs, i_x, i_nBs] = np.einsum('ij,j->ij', d['lep_tf'][0][0] + d['lep_tf'][0][1], phot_bin_width) # N -> N
+                lee_tf[i_rs, i_x, i_nBs] = np.einsum('ij,j->ij', d['lee_tf'][0][0] + d['lee_tf'][0][1], elec_bin_width) # N -> N
+                hed_tf[i_rs, i_x, i_nBs] = d['hed_tf'][0][0]*d['dt'][0][0] + d['hed_tf'][0][1]*d['dt'][0][1] # N -> E
+                cmbloss[i_rs, i_x, i_nBs] = d['cmbloss'][0][0]*d['dt'][0][0] + d['cmbloss'][0][1]*d['dt'][0][1] # N -> E
+                lowerbound[i_rs, i_x, i_nBs] = d['lowerbound'][0][1]
+                dt[i_rs] = d['dt'][0] # [step 0, step 1]
                 
                 pbar.update()
         
@@ -61,3 +64,4 @@ np.save(f'{save_dir}/lee_tf_rxneo.npy', lee_tf)
 np.save(f'{save_dir}/hed_tf_rxneo.npy', hed_tf)
 np.save(f'{save_dir}/cmbloss_rxneo.npy', cmbloss)
 np.save(f'{save_dir}/lowerbound_rxneo.npy', lowerbound)
+np.save(f'{save_dir}/dt_rxneo.npy', dt)

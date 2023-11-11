5804626a04029d010a936575cb91f4e70f6d34e3
Yitian Sun <yitian.sun.26@gmail.com>
2023-11-10 20:01:39 -0500

    per slice check working

--- step/prep_init.py
+++ step/prep_init.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     # p21c.global_params.NUM_FILTER_STEPS_FOR_Ts = 40
 
     return_dict = evolve(
-        run_name = f'stepinit_xdecay_ion_z10',
+        run_name = f'stepinit_dc_xdecay_ion_z10',
         z_start = 45.,
         z_end = 5.,
         zplusone_step_factor = 1.001,
@@ -51,14 +51,14 @@ if __name__ == '__main__':
 
         clear_cache = True,
         use_tqdm = True,
+        debug_break_after_z = 10.,
+        debug_record_extra = False,
         
+        # 21cmFAST xray injection
         use_21cmfast_xray = False,
         debug_turn_off_pop2ion = True,
         debug_xray_Rmax_p21c = 500.,
 
-        debug_break_after_z = 10.,
-        debug_record_extra = False,
-        
         # DM21cm xray injection
         debug_flags = ['xc-ots', 'xc-custom-SFRD', 'xc-01attenuation'],
         debug_unif_delta_dep = True,
@@ -67,4 +67,5 @@ if __name__ == '__main__':
         debug_nodplus1 = True,
         debug_xray_Rmax_shell = 500.,
         debug_xray_Rmax_bath = 500.,
+        adaptive_shell = 40,
     )

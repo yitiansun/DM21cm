ce4c5661aed6e615b20dd5933e4378c781bb4899
Yitian Sun <yitian.sun.26@gmail.com>
2024-03-05 14:45:16 -0500

    test pbh

---
+++ tests/submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=pbh
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=32GB
+#SBATCH --time=0-08:00:00
+#SBATCH --output=/n/home07/yitians/dm21cm/DM21cm/outputs/slurm/%x_%a.out
+#SBATCH --error=/n/home07/yitians/dm21cm/DM21cm/outputs/slurm/%x_%a.err
+#SBATCH --account=iaifi_lab
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yitians@mit.com
+
+source /n/home07/yitians/setup_dm21cm.sh
+
+cd /n/home07/yitians/dm21cm/DM21cm/tests
+
+python test_pbh.py --run_name off --f_PBH 1e-10
---
+++ tests/test_pbh.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import time
+import argparse
+
+import numpy as np
+
+from astropy.cosmology import Planck18
+import py21cmfast as p21c
+
+WDIR = os.environ['DM21CM_DIR']
+sys.path.append(WDIR)
+from dm21cm.injections.pbh import PBHInjection
+from dm21cm.evolve import evolve
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Test PBH injection')
+    parser.add_argument('--run_name', type=str, default='test_pbh', help='Run name')
+    parser.add_argument('--f_PBH', type=float, default=1e-8, help='PBH fraction')
+    args = parser.parse_args()
+
+    run_name = args.run_name
+
+    os.environ['P21C_CACHE_DIR'] = '/n/home07/yitians/21cmFAST-cache' # tmp
+    os.environ['P21C_CACHE_DIR'] = '/n/holyscratch01/iaifi_lab/yitians/21cmFAST-cache'
+    os.environ['DM21CM_DATA_DIR'] = '/n/holyscratch01/iaifi_lab/yitians/dm21cm/DM21cm/data/tf/zf002/data'
+
+    # set global params
+    p21c.global_params.CLUMPING_FACTOR = 1.
+    #p21c.global_params.Pop2_ion = 0.
+
+    return_dict = evolve(
+        run_name = run_name,
+        z_start = 45.,
+        z_end = 5.,
+        injection = PBHInjection(
+            m_PBH=1e15, # [g]
+            f_PBH=args.f_PBH,
+        ),
+        
+        p21c_initial_conditions = p21c.initial_conditions(
+            user_params = p21c.UserParams(
+                HII_DIM = 32,
+                BOX_LEN = 256, # [conformal Mpc]
+                N_THREADS = 32,
+            ),
+            cosmo_params = p21c.CosmoParams(
+                OMm = Planck18.Om0,
+                OMb = Planck18.Ob0,
+                POWER_INDEX = Planck18.meta['n'],
+                SIGMA_8 = Planck18.meta['sigma8'],
+                hlittle = Planck18.h,
+            ),
+            random_seed = 54321,
+            write = True,
+        ),
+        p21c_astro_params = p21c.AstroParams(), # log10 value
+        
+        resume = False,
+        subcycle_factor = 10,
+        max_n_shell = 10,
+    )
+
+    return_dict['lightcone']._write(fname=f'{run_name}_lightcone.h5', direc='.', clobber=True)
+    print(f'Generated lightcone.')
